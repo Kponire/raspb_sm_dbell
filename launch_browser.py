c29a31bb17b7481fd5244607ee8874c818355cd0
Kuponiyi Oluwaponire <kponire@gmail.com>
2026-02-01 16:15:05 +0100

    little update

--- launch_browser.py
+++ launch_browser.py
@@ -22,6 +22,35 @@ def start_chromium():
         "http://localhost:5000"
     ], env={**os.environ, "DISPLAY": ":0"})
 
+def test_start_chromium():
+    """Launch Chromium in kiosk mode pointing to our Flask app"""
+    global chromium_process
+    subprocess.run(["pkill", "-f", "chromium"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    chromium_process = subprocess.Popen([
+        "chromium-browser",
+        "--kiosk",
+        "--noerrdialogs",
+        "--disable-infobars",
+        "--no-first-run",
+        "--no-default-browser-check",
+        "--disable-sync",
+        "--disable-background-networking",
+        "--disable-renderer-backgrounding",
+        "--disable-background-timer-throttling",
+        "--disable-backgrounding-occluded-windows",
+        "--disable-features=TranslateUI",
+        "--disable-features=BackForwardCache",
+        "--disable-dev-shm-usage",
+        "--disable-gpu-vsync",
+        "--enable-low-end-device-mode",
+        "--use-gl=egl",
+        "--enable-accelerated-2d-canvas",
+        "--disable-features=UseOzonePlatform",
+        "--autoplay-policy=no-user-gesture-required",
+        "http://127.0.0.1:5000"
+    ], env={**os.environ, "DISPLAY": ":0"})
+
 def stop_chromium():
     """Kill Chromium when Python exits"""
     global chromium_process
